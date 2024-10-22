f3bd4b40e2ef6d0106fd9a065e90e7ca771aaea2
david65497881 <qaztgb818@gmail.com>
2024-10-23 00:54:40 +0800

    加入專案檔案。

---
+++ NLogDemo1_1734/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+
+
+
+namespace NLogDemo1_1734
+{
+    public class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Info("這是一條 Info 訊息");
+            logger.Error("這是一條 Error 訊息");
+
+            Console.WriteLine("Log 寫入完成");
+            Console.ReadLine();
+        }
+    }
+}
